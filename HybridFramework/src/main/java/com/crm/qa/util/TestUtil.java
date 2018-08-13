a89699e36c88c5f4b76f113b2a2750e6003862ff
Sampath <Sampath@Sampath-PC>
2018-08-13 13:41:36 +0530

    # WARNING: head commit changed in the meantime
    commit

--- HybridFramework/src/main/java/com/crm/qa/util/TestUtil.java
+++ HybridFramework/src/main/java/com/crm/qa/util/TestUtil.java
@@ -2,7 +2,7 @@ package com.crm.qa.util;
 
 public class TestUtil {
 
-	// Utility for global variables for commiting
+	// Utility for global variables 
 	public static long PAGE_LOAD_TIMEOUT=20;
 	public static long IMPLICIT_WAIT=20;
 	
