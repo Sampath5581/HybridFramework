939221a443a67bee3eb06c654c940ed526485159
Sampath5581 <40229064+Sampath5581@users.noreply.github.com>
2018-08-13 13:32:42 +0530

    updated
    ud

--- HybridFramework/src/main/java/com/crm/qa/util/TestUtil.java
+++ HybridFramework/src/main/java/com/crm/qa/util/TestUtil.java
@@ -2,7 +2,7 @@ package com.crm.qa.util;
 
 public class TestUtil {
 
-	// Utility for global variables
+	// Utility for global variables for commiting
 	public static long PAGE_LOAD_TIMEOUT=20;
 	public static long IMPLICIT_WAIT=20;
 	
